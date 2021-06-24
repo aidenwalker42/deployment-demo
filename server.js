13b985cbae00607235caa4cd63d4551a36431e0e
aidenwalker42 <khoul.pixel@gmail.com>
2021-06-24 15:10:34 -0500

    appuse added

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require("express");
 const app = express();
 
 const path = require("path")
-
+app.use(express.static(__dirname + "/public"));
 app.get("/", function(req, res){
 
     res.sendFile( path.join(__dirname, "./index.html") );
     
 })
 // app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname));
+
 
 const port = process.env.PORT || 5050;
 
